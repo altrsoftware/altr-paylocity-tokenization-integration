99c20778624b3aaa3232278b99f971aaa764493e
Noah Katz <noah@altr.com>
2023-02-01 15:45:57 -0500

    fix: fixed tokenization not being performed upon both arrays and objects

--- utils/main.js
+++ utils/main.js
@@ -18,28 +18,26 @@ const main = async (config) => {
 
     let _function = getFunction(config);
 
-    console.log("Sending request: " + config.Function);
-
     let res = (await sendGetRequest(auth, _function)).data;
     
-    const data = [];
+    const data = {};
     for(let item in res) {
         for(let tokenizeIndex in config.Tokenize) {
-            console.log("Tokenizing " + config.Tokenize[tokenizeIndex])
             const toTokenize = config.Tokenize[tokenizeIndex];
+            if(!(res[item].hasOwnProperty(toTokenize) || res.hasOwnProperty(toTokenize))) continue;
             try {
-                res[item][toTokenize] = (await tokenize({[toTokenize]: res[item][toTokenize]}))[toTokenize];
+                if(res[item].hasOwnProperty(toTokenize)) res[item][toTokenize] = (await tokenize({[toTokenize]: res[item][toTokenize]}))[toTokenize];
+                else res[toTokenize] = (await tokenize({[toTokenize]: res[toTokenize]}))[toTokenize];
             } catch(e) {
                 res[item][toTokenize] = "tokenization failed";
                 console.error('failed to tokenize ' + toTokenize);
             }
         }
-        data.push(res[item]);
+        data[item] = res[item];
     }
 
     if(config.Export === "true") {
-        const exportSuccess = await s3Export({body: data, name: `${config.Function}-${new Date().toISOString()}.json`});
-        console.log(exportSuccess);
+        await s3Export({body: data, name: `${config.Function}-${new Date().toISOString()}.json`});
     }
 
     return data;
