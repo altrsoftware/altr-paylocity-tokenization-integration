fd570d5a7f0d281267e401752bad79f646e1a1e4
Noah Katz <noah@altr.com>
2023-02-08 13:11:39 -0500

    feat: mainAllEmployees will aggregate all fields to be tokenized into one flattened object, and send to ALTR's vault tokenization API in bulk, rather than tokenizing values one by one

--- utils/mainAllEmployees.js
+++ utils/mainAllEmployees.js
@@ -1,6 +1,68 @@
-const getFunction = require('./functions');
+const getFunction = require('./functions').getFunction;
 const sendGetRequest = require('./sendGetRequest');
 const main = require('./main');
+const tokenize = require('./tokenize');
+const s3Export = require('./s3Export');
+
+function findKeyPath(o, s) {
+
+    let result = [];
+
+    function search(obj, key, path) {
+        for (let k in obj) {
+            if (key.includes(k)) {
+                result.push([...path, k]);
+            }
+            if (typeof obj[k] === "object") {
+                search(obj[k], key, [...path, k]);
+            }
+        }
+    }
+  
+    search(o, s, []);
+    return result;
+
+}
+
+async function toTokens(data, tokenizeKeys) {
+
+    if(typeof data !== "object") throw "Data must be type Object";
+    if(!Array.isArray(tokenizeKeys)) throw "Keys to tokenize must be type Array";
+
+    const pathArray = findKeyPath(data, tokenizeKeys);
+    let flatObj = {};
+    
+    pathArray.forEach(path => {
+        let current = data;
+        for (let i = 0; i < path.length; i++) {
+            const key = path[i];
+            current = current[key];
+        }
+        flatObj[path.join(".")] = current;
+    });
+    
+    try {
+        flatObj = await tokenize(flatObj);
+    } catch(e) {
+        Object.keys(flatObj).forEach(key => {
+            flatObj[key] = "tokenization failed"; //JSON.parse(JSON.stringify(e))// "tokenization failed";
+        })
+    }
+  
+    const newObj = JSON.parse(JSON.stringify(data));
+    Object.entries(flatObj).forEach(([path, value]) => {
+        let current = newObj;
+        const pathArray = path.split(".");
+        for (let i = 0; i < pathArray.length - 1; i++) {
+            const key = pathArray[i];
+            current = current[key];
+        }
+        current[pathArray[pathArray.length - 1]] = value;
+    });
+  
+    return newObj;
+
+}
 
 /**
  * Branches between requesting information for all employees or a single employee
@@ -45,16 +107,30 @@ const mainAllEmployees = async (config) => {
     }
 
     const tmpExport = config?.Export;
+    const tmpTokenize = config?.Tokenize;
+    config.Export = "false";
 
     const data = {};
 
+    // let employeeInit = config?.Parameters?.pageSize ?? 0 * config?.Parameters?.pageNum ?? 0;
+
     for(let employeeIndex in employees) {
+        if(employeeIndex > 10) break;
+        console.log(employeeIndex);
         config.Parameters.employee = employees[employeeIndex].employeeId;
-        config.Export = 'false';
+        config.Tokenize = [];
         data[employees[employeeIndex].employeeId] = await main(config);
     }
 
-    return data;
+    const obj = tmpTokenize.length !== 0 ? await toTokens(data, tmpTokenize, tokenize) : data;
+
+    if(tmpExport === "true") {
+        try {
+            await s3Export({body: data, name: `${config.Function}-${new Date().toISOString()}.json`});
+        } catch(e) {}
+    }
+
+    return obj;
 }
 
 module.exports = mainAllEmployees;
